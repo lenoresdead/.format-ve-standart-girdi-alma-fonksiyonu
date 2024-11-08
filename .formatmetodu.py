389ebbf733d9b8aeed5878b9a5f77a4bfcfc942e
goxcen <yilmazgokcen28@gmail.com>
2024-11-08 16:04:16 +0300

    Update .formatmetodu.py

--- .formatmetodu.py
+++ .formatmetodu.py
@@ -1,1 +1,11 @@
+x,y,z,d,e=1,2,3,4,5
 
+print("x degiskeninin degeri: {}".format(x))
+print("x: {} y: {} z: {} d: {} e: {}".format(x,y,z,d,e))
+
+print("{isim} {soyad}".format(isim=10,soyad=15))
+
+print("{0} {1}".format("a","b"))
+
+yas=input("lutfen yas giriniz:")
+print("kullanicinin girdigi deger:{}".format(yas))
